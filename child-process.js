68b8e6f7d9fbff6da008bcb0a278543a5ed7736e
Santosh Mahto <mahtos480@gmail.com>
2021-07-11 12:09:14 +0530

    learning child process

--- child-process.js
+++ child-process.js
@@ -1,5 +1,14 @@
-import { spawn } from 'child_process'
-const ls = spawn('ls', ['-lh', '/usr'])
+import { exec, execFile, spawn } from 'child_process'
+import { join } from 'path/posix'
+import { promisify } from 'util'
+
+/**
+ * ***************** spawn *****************
+ * it's return ChildProcess Interface
+ */
+
+// read the list of user
+const ls = spawn('ls', ['-lh', '/home'])
 
 ls.stdout.on('data', (data) => {
   console.log(`stdout: ${data}`)
@@ -11,4 +20,40 @@ ls.stderr.on('data', (data) => {
 
 ls.on('close', (code) => {
   console.log(`child process exited with code ${code}`)
+})
+
+/**
+ * ***************** exec *****************
+ * take callback
+ * we can make it promise
+ */
+
+// exec with callback
+exec('pwd', { cwd: join(process.cwd(), '..') }, (error, stdout, stderr) => {
+  if (error) throw error
+  if (stdout) console.log(`stdout: ${stdout}`)
+  if (stderr) console.error(`stderr: ${stderr}`)
+});
+
+// exec with promise [using IIFE]
+(async () => {
+  try {
+    const { stderr, stdout } = await promisify(exec)('pwd', { cwd: join(process.cwd(), '..') })
+    if (stdout) console.log(`stdout: ${stdout}`)
+    if (stderr) console.warn(`stderr: ${stderr}`)
+  } catch (error) {
+    console.error(`error: ${error.message}`)
+  }
+})()
+
+/**
+ * ***************** exec *****************
+ * same as exec But it will take executable file and list of arguments
+ * we can make it promise
+ */
+execFile('node', ['--version'], (error, stdout, stderr) => {
+  if (error) {
+    throw error
+  }
+  console.log('Nodejs version:-', stdout)
 })
